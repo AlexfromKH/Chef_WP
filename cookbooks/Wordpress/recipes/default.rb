62756db6468386c671252b824fd5a5aaf31b0214
AlexfromKH <AlexfromKH@yahoo.com>
2018-07-17 13:52:49 +0200

    create a test file under home directory

--- cookbooks/Wordpress/recipes/default.rb
+++ cookbooks/Wordpress/recipes/default.rb
@@ -3,3 +3,8 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+file "/home/ansible/test_chef.txt" do
+  content "Test file, to ensure chef-solo runs properly"
+  action :create
+end
